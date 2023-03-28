97bb0729b1809645b8954173409dbeaf6efbd618
KeelanAnderson <keelan.anderson.ka@gmail.com>
2023-03-28 16:52:38 +0000

    add html for posts in index.html

--- booking/views.py
+++ booking/views.py
@@ -7,4 +7,4 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("created_on")
     template_name = "index.html"
-    paginate_by = 8
+    paginate_by = 6
